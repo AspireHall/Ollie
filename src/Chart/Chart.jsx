a06b5afcb6d60ccd32eef4038f7a1783913da9e3
Namagemx <106352841+Namagemx@users.noreply.github.com>
2024-09-01 23:58:51 -0500

    added chart

--- src/Chart/Chart.jsx
+++ src/Chart/Chart.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import classes from "./Chart.module.css";
 
 const Chart = () => {
-  const src = "";
+  const src =
+    "https://dexscreener.com/solana/CD4LNnrw822QoyddCT6qkghdWYecQDu8SuSfTtF7oFCX?embed=1&theme=dark";
 
   return (
     <div>
