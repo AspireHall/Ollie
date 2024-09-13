08b1f8ab89113382fe8e7890504b4c1e48120dca
Namagemx <106352841+Namagemx@users.noreply.github.com>
2024-09-13 04:45:15 -0500

    use api code string instead of env

--- src/Features/coinLayerApi.js
+++ src/Features/coinLayerApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const accessKey = process.env.REACT_APP_COINLAYER_API_KEY;
+const envAccessKey = process.env.REACT_APP_COINLAYER_API_KEY;
+const accessKey = "301b6086caaa44ae88de4510e2e9343b";
 
 // Define a service using a base URL and expected endpoints
 export const coinLayerApi = createApi({
