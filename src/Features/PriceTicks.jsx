19ccb2aace9f528c7b42e0dc2af307227e8b602d
Namagemx <106352841+Namagemx@users.noreply.github.com>
2024-09-14 01:32:57 -0500

    correcting data

--- src/Features/PriceTicks.jsx
+++ src/Features/PriceTicks.jsx
@@ -4,42 +4,50 @@ import { useGetPriceByCoinQuery } from "./coinLayerApi";
 
 const PriceTicks = () => {
   const { data, error, isLoading } = useGetPriceByCoinQuery();
-  /*
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  console.log(data);
 
-  if (error) {
-    return <div>Error...</div>;
-  }
-
-  let btc = data?.rates.BTC.toFixed(0).toLocaleString("en-US");
-  let eth = data?.rates.ETH.toFixed(0).toLocaleString("en-US");
-  let bnb = data?.rates.BNB.toFixed(0).toLocaleString("en-US");
-  let sol = data?.rates.SOL.toFixed(0).toLocaleString("en-US");
-  let xrp = data?.rates.XRP.toFixed(3).toLocaleString("en-US");
-  let ada = data?.rates.ADA.toFixed(3).toLocaleString("en-US");
-  let doge = data?.rates.DOGE.toFixed(3).toLocaleString("en-US");
-  let avax = data?.rates.AVAX.toFixed(2).toLocaleString("en-US");*/
   return (
     <div className={classes.mainContainer}>
-      {/* <h1 className={classes.symbolText}>BTC</h1>
-      <h1 className={classes.priceText}>${btc}</h1>
-      <h1 className={classes.symbolText}>ETH</h1>
-      <h1 className={classes.priceText}>${eth}</h1>
-      <h1 className={classes.symbolText}>BNB</h1>
-      <h1 className={classes.priceText}>${bnb}</h1>
-      <h1 className={classes.symbolText}>SOL</h1>
-      <h1 className={classes.priceText}>${sol}</h1>
-      <h1 className={classes.symbolText}>XRP</h1>
-      <h1 className={classes.priceText}>${xrp}</h1>
-      <h1 className={classes.symbolText}>ADA</h1>
-      <h1 className={classes.priceText}>${ada}</h1>
-      <h1 className={classes.symbolText}>DOGE</h1>
-      <h1 className={classes.priceText}>${doge}</h1>
-      <h1 className={classes.symbolText}>AVAX</h1>
-      <h1 className={classes.priceText}>${avax}</h1>
-      */}
+      {error ? (
+        <>Oh, no there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h1 className={classes.symbolText}>BTC</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.BTC.toFixed(0).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>ETH</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.ETH.toFixed(0).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>BNB</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.BNB.toFixed(0).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>SOL</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.SOL.toFixed(0).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>XRP</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.XRP.toFixed(3).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>ADA</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.ADA.toFixed(3).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>DOGE</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.DOGE.toFixed(3).toLocaleString("en-US")}
+          </h1>
+          <h1 className={classes.symbolText}>AVAX</h1>
+          <h1 className={classes.priceText}>
+            ${data?.rates.AVAX.toFixed(2).toLocaleString("en-US")}
+          </h1>
+        </>
+      ) : null}
     </div>
   );
 };
